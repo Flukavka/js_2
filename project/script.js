01560e521419405b5ce667958db4551b0fd0a41b
Victoriia <nosareva.vs@gmail.com>
2022-05-31 10:35:26 +0300

    homework for the third lesson

--- project/script.js
+++ project/script.js
@@ -5,15 +5,31 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ];
 
+const BASA_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const GET_GOODS_ITEMS_URL = `${BASA_URL}/catalogData.json`;
+const GET_BASKET_URL = `${BASA_URL}/getBasket.json`;
+
+function service(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url)
+  const loadHandler = () => {
+    callback(JSON.parse(xhr.response));
+  }
+  xhr.onload = loadHandler;
+
+  xhr.send();
+}
+
+
 class GoodsItem {
-  constructor({ title = 'Default', price = 0 }) {
-    this.title = title;
+  constructor({ product_name = 'Default', price = 0 }) {
+    this.product_name = product_name;
     this.price = price;
   }
   render() {
     return `
     <div class="goods-item">
-      <h3>${this.title}</h3>
+      <h3>${this.product_name}</h3>
       <div class="goods-itemImg"></div>
       <p>${this.price}</p>
     </div>
@@ -22,8 +38,12 @@ class GoodsItem {
 }
 
 class GoodsList {
-  fetchData() {
-    this.list = goods;
+  list = [];
+  fetchData(callback) {
+    service(GET_GOODS_ITEMS_URL, (data) => {
+      this.list = data;
+      callback();
+    });
   }
 
   render() {
@@ -35,13 +55,24 @@ class GoodsList {
   }
 
   getTotalPrice() {
-    const totalPrice = goods.reduce((total, goods) =>
-      total + goods.price, 0);
-    console.log(totalPrice);
+    return goods.reduce((total, goods) => total + goods.price, 0);
   }
 }
 
+class BasketGoods {
+  list = [];
+  fetchData(callback = () => { }) {
+    service(GET_BASKET_URL, (data) => {
+      this.list = data;
+      callback();
+    })
+  };
+}
+
 const goodsList = new GoodsList(goods);
-goodsList.fetchData()
-goodsList.render()
-goodsList.getTotalPrice()
+goodsList.fetchData(() => {
+  goodsList.render();
+});
+
+const basketGoods = new BasketGoods();
+basketGoods.fetchData();
