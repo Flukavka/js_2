5cbc36a2c9c3abd97ce59ee79314173a320f05b6
Victoriia <nosareva.vs@gmail.com>
2022-06-14 15:11:41 +0300

    Выполнено домашнее задание к шестому уроку.

--- project/script.js
+++ project/script.js
@@ -8,14 +8,14 @@ async function service(url) {
   return fetch(url).then((res) => res.json());
 }
 
-class BasketGoods {
+/* class BasketGoods {
   items = [];
   fetchData() {
-    service(GET_BASKET_URL, (data) => {
+    return service(GET_BASKET_URL, (data) => {
       this.items = data;
     });
   }
-}
+} */
 
 
 window.onload = () => {
@@ -24,7 +24,6 @@ window.onload = () => {
     data: {
       items: [],
       searchValue: '',
-      /* cardVision: false, */
     },
     mounted() {
       service(GET_GOODS_ITEMS_URL).then((data) => {
@@ -57,17 +56,81 @@ window.onload = () => {
         });
       },
     },
-
   })
+};
 
-  const goodsItem = Vue.component('goods-item', {
-    props: ['item'],
-    template: `
-    <div class="goods-item">
-               <h3>{{item.product_name}}</h3>
-               <div class="goods-itemImg"></div>
-               <p>{{item.price}}</p>
+const goodsItem = Vue.component('goods-item', {
+  props: ['item'],
+  template: `
+  <div class="goods-item">
+             <h3>{{item.product_name}}</h3>
+             <div class="goods-itemImg"></div>
+             <p>{{item.price}}</p>
+          </div>
+  `
+});
+
+const goodsSearh = Vue.component('searh', {
+  template: `
+  <input type="text" class="search_text" placeholder="Наименование продукта" @input="$emit('input', $event.target.value)"/>
+  `},
+  {
+    computed: {
+      filteredItems() {
+        return this.items.filter(({ product_name }) => {
+          return (new RegExp(this.searchValue, 'gui')).test(product_name);
+        })
+      }
+    }
+  });
+
+const goodsBasket = Vue.component('basket', {
+  data() {
+    return {
+      basketGoodsItem: []
+    }
+  },
+  mounted() {
+    service(GET_BASKET_URL).then((data) => {
+      this.items = data;
+    });
+  },
+  template: `
+  <div class="basket">
+               <div class="hidden basket_hidden">
+                  <div class="basket_headerBlock">
+                     <div class="basket_headerBlock__main">
+                        <div class="basket_headerBlock__mainHeadding">Корзина</div>
+                        <div class="basket_headerBlock__mainClose"><img src="img/closePurple.svg" alt="close"></div>
+                     </div>
+                     <div class="basket_headerBlock__goods">
+                        <div class="basket_headerBlock__goodsName">Наименование товара</div>
+                        <div class="basket_headerBlock__goodsPrice">Стоимость</div>
+                        <div class="basket_headerBlock__goodsCount">Количество</div>
+                     </div>
+                  </div>
+                  <div class="basket_userBlock">
+                     <!-- <div class="basket_userBlock__productName">Default</div>
+                  <div class="basket_userBlock__productPrice">
+                     <span class="basket_userBlock__productPriceValue">0</span>
+                     <span class="basket_userBlock__productPriceValute">₽</span>
+                  </div>
+                  <div class="basket_userBlock__productCount">
+                     <input class="basket_userBlock__productCountValue" placeholder="1" type="number">
+                     <span class="basket_userBlock__productCountDesignation">шт</span>
+                  </div>
+                  <button class="basket_userBlock__deleteBtn">Удалить</button> -->
+                     <div class="basket_userBlock__emptyBasket">Список товаров пуст</div>
+                  </div>
+                  <div class="basket_totalBlock">
+                     <div class="basket_totalBlock__headding">Итого</div>
+                     <div class="basket_totalBlock__price">
+                        <span class="basket_totalBlock__priceValue">0</span>
+                        <span class="basket_totalBlock__priceValute">₽</span>
+                     </div>
+                  </div>
+                  <button class="basket_orderBtn">Заказать</button>
+               </div>
             </div>
-    `
-  })
-}
+  `
+})
